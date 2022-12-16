2bb2d091fd9f93d345c48d8cdbed07435dec403c
taysir <alamt10@mcmaster.ca>
2022-12-16 00:55:11 -0500

    small edits

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def Apps():
     apps = [
         {
-            'name: Facebook'
-            'image': 'facebook.png' 
+            'name': 'Facebook',
+            'image': 'facebook.png' ,
             'description': 'This is the mobile link for facebook'
-        }
+        },
         {
-            'name: Snapchat'
-            'image': 'snap.png' 
+            'name': 'Snapchat',
+            'image': 'snap.png' ,
             'description': 'This is the mobile link for Snapchat'
-        }
+        },
         {
-            'name: Instagram'
-            'image': 'instagram.png' 
+            'name': 'Instagram',
+            'image': 'instagram.png',
             'description': 'This is the mobile link for Instagram'
         }
     ]
